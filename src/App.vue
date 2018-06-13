c658dd164b8b1ea3a848912f22fc7c2d2acd0714
Hichem Fend <simplizine@gmail.com>
2018-06-13 08:34:19 +0100

    Port Vue.js SPA #1

---
+++ src/App.vue
@@ -0,0 +1,128 @@
+<template>
+  <div id="app">
+
+    <section class="section">
+      <div class="container">
+        <h1 class="title">
+          Barcode Generator
+        </h1>
+        <p class="subtitle">
+          Generate Barcodes (currently QR codes) from <strong>Text</strong> or <strong>Base64</strong> payloads!
+        </p>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+
+        <div class="tabs">
+          <ul>
+            <li :class="{'is-active': method=='text'}"><a @click.prevent="method='text'">From&nbsp;<strong>Text</strong></a></li>
+            <li :class="{'is-active': method=='base64'}"><a @click.prevent="method='base64'">From&nbsp;<strong>Base64</strong></a></li>
+          </ul>
+        </div>
+
+        <div class="columns">
+
+          <div class="column is-two-thirds">
+            <form id="payload-form" @submit.prevent="onSubmit">
+              <textarea class="textarea" v-if="method == 'text'" v-model="text" placeholder="Insert text payload.."></textarea>
+              <textarea class="textarea" v-else-if="method == 'base64'" v-model="base64" placeholder="Insert base64 payload.."></textarea>
+              <br/>
+              <button :class="{'button is-primary is-large full-width': true, 'is-loading': loading}" :disabled="disabled">
+                <span class="icon is-small">
+                  <i class="fas fa-qrcode"></i>
+                </span>
+                <span>Generate</span>
+              </button>
+            </form>
+          </div>
+
+          <div class="column">
+            <div id='result' class="notification" v-if="result != null">
+              <img :src="result" />
+              <br/>
+              <a class="button is-info" :href="result" download="qrcode.png">
+                <span class="icon is-small">
+                  <i class="fas fa-download"></i>
+                </span>
+                <span>Download</span>
+              </a>
+            </div>
+            <div id="error" class="notification is-danger" v-if="error != null">
+              <button class="delete" @click.prevent="error=null"></button>
+              {{error}}
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<script>
+import {Configs} from './configs.js'
+
+let QRCODE_SERVICE = (process.env.NODE_ENV === 'development' ? Configs.DEV_QRCODE_SERVICE : Configs.PROD_QRCODE_SERVICE)
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      method: 'text', // 'text' | 'base64'
+      text: '',
+      base64: '',
+      loading: false,
+      result: null,
+      error: null
+    }
+  },
+  computed: {
+    disabled: function () {
+      return !this.$data[this.$data.method] || this.$data.loading;
+    }
+  },
+  methods: {
+
+    onSubmit() {
+
+      this.$data.result = null
+      this.$data.error = null
+      this.$data.loading = true
+
+      let data = {}
+      data[this.$data.method] = this.$data[this.$data.method]
+
+      this.$http.post(QRCODE_SERVICE, data, { emulateJSON: true, responseType: 'arraybuffer' }).then(function(res){
+        if (res.status == 200) {
+          this.$data.result = `data:${res.headers.get('content-type')};base64,${btoa(String.fromCharCode.apply(null, new Uint8Array(res.data)))}`
+        }
+        this.$data.loading = false
+      }, function(err) {
+        if (err.status == 400 && err.body.description) {
+          this.$data.error = err.body.description
+        } else {
+          this.$data.error = 'Oops! Something went wrong.'
+        }
+        this.$data.loading = false
+      });
+
+    }
+  }
+}
+</script>
+
+<style>
+  #result {
+    text-align: center;
+  }
+  #error {
+    background-color: #dd0000;
+  }
+  .full-width{
+    width: 100%;
+  }
+</style>
---
+++ src/configs.js
@@ -0,0 +1,4 @@
+export const Configs = Object.freeze({
+  DEV_QRCODE_SERVICE: "http://localhost:9001",
+  PROD_QRCODE_SERVICE: "https://service.qrbarco.de",
+});
